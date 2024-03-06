16a9e2a4fdf43c996f608a1cd21c611454b01554
raovarun91 <30377012+raovarun91@users.noreply.github.com>
2024-03-06 23:55:52 +0400

    all functions

--- lesson9.js
+++ lesson9.js
@@ -36,3 +36,45 @@ const sum4 = (x,y) => {
 }
 let val2 = sum4(5,5)
 console.log(val2)
+
+//forEach function      array.forEach (callbackfunction)
+
+let ourscore = [3,5,1,9,6]
+
+ourscore.forEach(function increaseone(val) {
+    console.log(val + 1)
+})
+
+ourscore.forEach((val) => console.log(val+1))       //doing same using arrow funciton as callback function
+
+ourscore.forEach((val, idx, arr) => {               //3 parameter eachvalue, index, completearray could be passed to forEach funciton
+    console.log(val+1, idx, arr)
+})
+
+let oursamefunction = (val) => console.log(val+1)
+ourscore.forEach(oursamefunction)
+
+
+//map funciton - same as forEach but used when we want to create a new array
+let newscore = ourscore.map((val) => {
+    return val*2
+})
+console.log(newscore)
+
+
+//filter function
+let oddscore = ourscore.filter((val) => {
+    return val%2 ===1
+})
+console.log(oddscore)
+
+//reduce function   - takes array, perform opertaion and return single value. aggregator function
+let sumscore = ourscore.reduce((sum, val) => {
+    return sum + val
+})
+console.log(sumscore)
+
+let highscore = ourscore.reduce((high, val) => {
+    return high > val ? high : val
+})
+console.log(highscore)
